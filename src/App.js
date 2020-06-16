0cd3a96d7fe7a54b5948981520614c0d1a2455fa
Luan Tran <minhluanlqd@gmail.com>
2020-06-15 20:22:51 -0400

    create admin page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Admin, Resource } from 'react-admin';
 import './App.css';
+import { PostList, PostEdit, PostCreate} from './posts';
+import restProvider from 'ra-data-simple-rest';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <Admin dataProvider={restProvider('http://localhost:3000')}>
+        <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate}/>
+      </Admin>,
     </div>
   );
 }
