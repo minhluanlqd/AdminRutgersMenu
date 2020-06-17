cb843cbca2199d5b64674e283b0f7ca826e64dcf
Luan Tran <minhluanlqd@gmail.com>
2020-06-16 23:23:48 -0400

    add login page

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-import Table from '../components/tables/tables'
+import Table from '../components/tables/tables';
+import SignIn from '../components/signin/signin';
 
 function App() {
   return (
     <div className="App">
-         <Table/>
+         {/* <Table/> */}
+          <SignIn/>
     </div>
   );
 }
