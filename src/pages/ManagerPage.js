9d061f547031d1e49a3685bf6dc711017131977a
tan <nttrongtan1605@gmail.com>
2020-08-03 20:08:56 -0400

    clean redundant code

--- src/pages/ManagerPage.js
+++ src/pages/ManagerPage.js
@@ -4,7 +4,6 @@ import Table from "../components/tables/tables";
 const ManagerPage = (props) => (
   <div>
     <Table />
-    <button onClick={() => props.setUserState(null)}>Sign Out</button>
   </div>
 );
 
--- src/pages/SignInPage.js
+++ src/pages/SignInPage.js
@@ -4,7 +4,6 @@ import SignInComponent from "../components/signin/signin";
 const SignPage = (props) => (
   <div>
     <SignInComponent />
-    <button onClick ={() => props.setUserState('john')}>Sign In</button>
   </div>
 );
 
