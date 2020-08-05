402a15d65dc0ab3fc194054a7ed3c1329cc596a9
Luan Tran <minhluanlqd@gmail.com>
2020-08-04 21:24:58 -0400

    Updates

---
+++ precache-manifest.2e03b1b87e5de1db14875ea8b3383088.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "501b94c6f57979d47ecf249c3cf09c21",
+    "url": "/AdminRutgersMenu/index.html"
+  },
+  {
+    "revision": "fea5138f0e2f8a75784d",
+    "url": "/AdminRutgersMenu/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ba46aa9f2fd0929e9129",
+    "url": "/AdminRutgersMenu/static/css/main.60f9e1d7.chunk.css"
+  },
+  {
+    "revision": "fea5138f0e2f8a75784d",
+    "url": "/AdminRutgersMenu/static/js/2.d863c5e2.chunk.js"
+  },
+  {
+    "revision": "0bf38cf6c0b51ec69d7a52e8875a6dbf",
+    "url": "/AdminRutgersMenu/static/js/2.d863c5e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba46aa9f2fd0929e9129",
+    "url": "/AdminRutgersMenu/static/js/main.a92063df.chunk.js"
+  },
+  {
+    "revision": "6cb05e799ac622adbb23",
+    "url": "/AdminRutgersMenu/static/js/runtime-main.310cd3bc.js"
+  }
+]);
